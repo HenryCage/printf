be7dcee2e5d405124d5027d6d3288e0a238354aa
Omo-Bare Emmanuel <henry12cage@gmail.com>
2023-10-20 18:53:28 -0700

    Hmmm

--- main.h
+++ main.h
@@ -7,4 +7,6 @@ int print_char(va_list c);
 int _strlen(char *s);
 int print_str(va_list h);
 int print_sig(void);
+int print_decimal(va_list d);
+int print_int(va_list d);
 #endif
---
+++ print_int.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_int - print interger with %d specifier
+ * @d: the argment
+ * Return: the number to be printed
+ */
+int print_int(va_list d)
+{
+	int n = va-args(args, int);
+	int number, a = n % 10;
+	int b, x = 1;
+	int i = 1;
+
+	n = n/ 10;
+	number = n;
+
+	if (a < 0)
+	{
+		_putchar('-');
+		number = -number;
+		n = -n;
+		a = -a;
+		i++;
+	}
+
+	if (number > 0)
+	{
+		while (number / 10 != 0)
+		{
+			x = x * 10;
+			number = number / 10;
+		}
+
+		number = n;
+		while (x > 0)
+		{
+			b = number / x;
+			_putchar(b + '0');
+			number = number - (b * x);
+			x = x / 10;
+			i++
+		}
+	}
+	
+	_putchar(a + '0');
+	return (i);
+}
